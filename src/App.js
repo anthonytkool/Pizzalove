de26c49d45744ebc57b593ef578a93c17a58b388
Anthony Cool <anthonytcool@hotmail.com3>
2022-05-02 17:31:41 +0700

    Add Google Fonts, installed styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css'
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobayStyle = createGlobalStyle`
+body{
+  font-family: 'Radio Canada', sans-serif;
+}
+h1, h2, h3 {
+  font-family: 'Libre Bodoni', serif;
+}
+`
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Pizza Lover</h1>
-    </div>
+    <>
+      <GlobayStyle />
+      <h1>Pizza Love</h1>
+      <p>Hello! Pizza Loverr</p>
+    </>
   )
 }
 
