b47fc2b469cc97be633794c1ab1ec80c740f3eeb
Anthony Cool <anthonytcool@hotmail.com3>
2022-05-05 09:25:29 +0700

    Add Favicon  and UseTitle

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import { GlobalStyle } from './Styles/GlobalStyle'
 import { Order } from './Order/Order'
 import { useOpenFood } from './Hooks/useOpenFood'
 import { useOrders } from './Hooks/useOrders'
+import { useTitle } from './Hooks/useTitle'
 
 function App() {
   const openFood = useOpenFood()
-  const order = useOrders()
+  const orders = useOrders()
+  useTitle({...openFood, ...orders})
   return (
     <>
       <GlobalStyle />
-      <FoodDialog {...openFood} {...order} />
+      <FoodDialog {...openFood} {...orders} />
       <Navbar />
-      <Order {...order} />
+      <Order {...orders} />
       <Banner />
       <Menu {...openFood} />
     </>
